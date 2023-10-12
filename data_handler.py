e732e27034dbd0be24e814853ba27eb5ae40daaa
“hussein” <hussein.hajhassan1@net.usj.edu.lb>
2023-10-12 10:22:12 +0300

    fixed code

--- data_handler.py
+++ data_handler.py
@@ -16,3 +16,22 @@ def read_file(file_path):
         suffix = str(error)
         error_handler.print_error(suffix,prefix)
         return None
+    
+
+def return_query(conn, query):
+    try:
+        with conn.cursor() as cur:
+            cur.execute(query)
+            result = cur.fetchall()
+            return result
+    except Exception as error:
+        prefix = lookups.ErrorHandling.Excute_query_error.value
+        suffix = str(error)
+        error_handler.print_error(suffix,prefix)
+
+
+    
+def return_sql_file(conn, file_path):
+    with open(file_path, 'r') as file:
+        sql_query = file.read()
+        return return_query(conn, sql_query)
--- database_handler.py
+++ database_handler.py
@@ -1,22 +1,20 @@
 import psycopg2
 import lookups
 import error_handler
+import json
 
 def connect_to_db(config_file):
     db_session = None
     try:
-
-        db_host = 'localhost'
-        db_name = 'Bank-data'
-        db_user = 'postgres'
-        db_pass = 'hajhaSSan1'
+        with open(config_file, 'r') as file:
+            config = json.load(file)
 
         db_session = psycopg2.connect(
-            host = db_host,
-            database = db_name,
-            user = db_user,
-            password = db_pass,
-            port = 5432
+            host=config["db_host"],
+            database=config["db_name"],
+            user=config["db_user"],
+            password=config["db_pass"],
+            port=config["db_port"]
         )
     except Exception as error:
         prefix = lookups.ErrorHandling.DB_CONNECTION_ERROR.value
@@ -24,3 +22,19 @@ def connect_to_db(config_file):
         error_handler.print_error(suffix, prefix)
     finally:
         return db_session
+    
+
+def execute_query(conn, query):
+    try:
+        with conn.cursor() as cur:
+            cur.execute(query)
+            conn.commit()
+    except Exception as error:
+        prefix = lookups.ErrorHandling.Excute_query_error.value
+        suffix = str(error)
+        error_handler.print_error(suffix,prefix)
+
+def execute_sql_file(conn, file_path):
+    with open(file_path, 'r') as file:
+        sql_query = file.read()
+        return execute_query(conn, sql_query)
