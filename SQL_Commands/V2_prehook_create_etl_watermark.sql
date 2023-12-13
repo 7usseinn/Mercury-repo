4399420cf477e477e68698174be08cfd1b4e6df6
7ussein <husseinhajhassan76@gmail.com>
2023-12-13 17:07:28 +0200

    .

--- SQL_Commands/V2_prehook_create_etl_watermark.sql
+++ SQL_Commands/V2_prehook_create_etl_watermark.sql
@@ -3,3 +3,6 @@ Create Table if NOT EXISTS bank_schema.etl_watermark
     id SERIAL PRIMARY KEY NOT NULL,
     etl_last_execution_time TIMESTAMP
 );
+
+INSERT INTO bank_schema.etl_watermark (etl_last_execution_time)
+VALUES ('1900-1-1 08:30:00');
